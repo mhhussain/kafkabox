af3fe5be88302fb42338068cc5e518cf45f8fb06
moohh <mo051191@yahoo.com>
2019-09-28 09:43:24 -0500

    working on streaming

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Kafkabox - Kafka Sandbox
-Sandbox Kafka - single broker and zookeeper
+Sandbox Kafka - single broker and zookeeper, and a nice little API as well.
 
 ## Purpose
 
@@ -12,17 +12,24 @@ Added a backend API (currently in node) at port 3001. Export port 3001 when crea
 
 ## Running the sandbox
 
-The main image can be found here: https://cloud.docker.com/repository/docker/moohh/kafkabox
+The main image can be found here: https://hub.docker.com/r/moohh/kafkabox
 
 To run the sandbox pull and run the image:
 
 ```
-docker pull moohh/kafkabox
-
 docker run -t --name kafkabox -p 9092:9092 -p 3001:3001 moohh/kafkabox
 ```
 
 Once the sandbox is running, you should be able to implement a Kafka publisher and consumer with a broker address of localhost:9092. Backend API is also available at port 3001.
+
+## Version
+
+- 1.0.0 - Working version of kafkabox docker image
+- 1.0.1 - Changed test-topic name to sandbox-topic
+- 2.0.0 - Added (node) backend api at port 3001
+- 2.1.0 - Added get topics endpoint and some error handling
+- 2.2.0 - Added a producer to send messages to topics in the sandbox
+- 2.3.0 - Added consumer - get all messages and get message by offset
 
 ## Kafka API
 
@@ -33,20 +40,18 @@ Current list of endpoints:
 |End point|Type|Details|
 |---|---|---|
 |_/topics_|GET|Get list of topics in sandbox|
-|_/createTopics_|POST|Create topic, must include `topicName` in `body` of request|
-|_/sendMessage_|POST|Puts a message on a topic, must include `message` and `topic` in `body` of request|
-
-
+|_/:topic/create_|POST|Create topic, must include `topicName` in `body` of request|
+|_/:topic/send_|POST|Adds a message to a topic, must include `message` and `topic` in `body` of request|
+|_/:topic_|GET|Gets all messages from `:topic`|
+|_/:topic/:offset_|GET|Gets message from `:topic` with offset `:offset`|
 
 ## Building from source
 
 To build the image run:
 
-`
-docker build .
-`
+`docker build .`
 
-## Details
+### Build and run details
 
 This is the current process in my mind.
 
@@ -62,7 +67,7 @@ When the docker image is created:
 9. Copy server start file
 
 When the image is run:
-`docker run -t --name container -p 9092:9092 moohh/kafkabox`
+`docker run -t --name container -p 9092:9092 -p 3001:3001 moohh/kafkabox`
 1. Run a basic zookeeper instance on port 2181  as a process in the image
 2. Run a basic broker instance on port 9092 as a process in the image
 3. Create a new topic called 'sandbox-topic'
@@ -79,12 +84,3 @@ Get container name:
 `docker ps`
 
 Update hosts file at: `/etc/hosts` and have the container name point to 127.0.0.1
-
-
-### Version
-
-- 1.0.0 - Working version of kafkabox docker image
-- 1.0.1 - Changed test-topic name to sandbox-topic
-- 2.0.0 - Added (node) backend api at port 3001
-- 2.1.0 - Added get topics endpoint and some error handling
-- 2.2.0 - Added send to topic, also some additional error handling
--- kafka-api/index.js
+++ kafka-api/index.js
@@ -1,19 +1,68 @@
-let e = require('express');
+let e = require('@feathersjs/express');
+let feathers = require('@feathersjs/feathers');
+let socketio = require('@feathersjs/socketio');
 let kafka = require('kafka-node');
 
-let app = e();
+let app = e(feathers());
 let configs = {
-    port: 3001
+    port: 3002
 };
 
-let KAFKA_HOST = '127.0.0.1:9092'
-let kClient = new kafka.KafkaClient({ kafkaHost: KAFKA_HOST });
+let KAFKA_HOST = '127.0.0.1:9092';
+
+/// **********************************************************************
+/// Feathers
+/// **********************************************************************
+class MessageService {
+    constructor() {
+      this.messages = [];
+    }
+  
+    async find () {
+      // Just return all our messages
+      return this.messages;
+    }
+  
+    async create (data) {
+      // Add new message to the list
+      this.messages.push(data);
+      return data;
+    }
+  }
 
 app.use(e.json());
 
+app.use(e.urlencoded({ extended: true }));
+
+app.use(e.static(__dirname));
+
+app.configure(e.rest());
+
+app.configure(socketio());
+
+app.use('/messages', new MessageService());
+
+app.use(e.errorHandler());
+
+app.on('connection', connection =>
+  app.channel('everybody').join(connection)
+);
+
+app.publish(data => app.channel('everybody'));
+
+// For good measure let's create a message
+// So our API doesn't look so empty
+app.service('messages').create({
+  text: 'Hello world from the server'
+});
+
+/// **********************************************************************
+/// Kafka Access
+/// **********************************************************************
 // Get list of topics
 app.get('/topics', (req, res) => {
-    let admin = new kafka.Admin(kClient);
+    let aClient = new kafka.KafkaClient({ kafkaHost: KAFKA_HOST });
+    let admin = new kafka.Admin(aClient);
     admin.listTopics((err, topics) => {
         if (err != null) {
             res.json(err);
@@ -24,16 +73,22 @@ app.get('/topics', (req, res) => {
 });
 
 // Create new topic with body { topicName }
-app.post('/createTopic', (req, res) => {
-    let { topicName } = req.body;
+app.post('/:topic/create', (req, res) => {
+    let topic = req.params.topic;
+
+    if (!topic) {
+        res.json("no topic specified");
+        return;
+    }
 
     let topics = [{
-        topic: topicName,
+        topic: topic,
         partitions: 1,
         replicationFactor: 1
     }];
 
-    let admin = new kafka.Admin(kClient);
+    let aClient = new kafka.KafkaClient({ kafkaHost: KAFKA_HOST });
+    let admin = new kafka.Admin(aClient);
     admin.createTopics(topics, (err, adResp) => {
         if (err != null) {
             res.json(err);
@@ -43,9 +98,10 @@ app.post('/createTopic', (req, res) => {
     });
 });
 
-// Send message to topic with body { topic, message }
-app.post('/sendMessage', (req, res) => {
-    let { message, topic } = req.body;
+// Send { message } from body to :topic
+app.post('/:topic/send', (req, res) => {
+    let topic = req.params.topic;
+    let { message } = req.body;
 
     if (message == null || topic ==  null) {
         res.json({error: "no message and/or topic"});
@@ -67,6 +123,99 @@ app.post('/sendMessage', (req, res) => {
     producer.on('error', (err) => {
         console.log(`error: ${err}`);
         res.json(err);
+    });
+});
+
+// Get all messages from :topic
+app.get('/:topic', (req, res) => {
+    let topic = req.params.topic;
+
+    if (!topic) {
+        res.json("no topic");
+        return;
+    }
+
+    let cClient = new kafka.KafkaClient({ kafkaHost: KAFKA_HOST });
+    let payload = [
+        {
+            topic: topic,
+            offset: 0,
+            partition: 0
+        }
+    ];
+    var options = {
+        autoCommit: false,
+        fetchMaxWaitMs: 3000,
+        fetchMaxBytes: 1024 * 1024,
+        fromOffset: true
+    };
+    
+    var messageArray = [];
+    
+    let consumer = new kafka.Consumer(cClient, payload, options);
+    consumer.on('message', (message) => {
+        messageArray.push(message);
+        app.service('messages').create(message);
+
+        if (message.highWaterOffset === message.offset + 1) {
+            res.json(messageArray);
+            consumer.close();
+        }
+    });
+
+    consumer.on('error', (err) => {
+        res.json(err);
+        consumer.close();
+    });
+});
+
+// Get the message at :offset from :topic
+app.get('/:topic/:offset', (req, res) => {
+    let topic = req.params.topic;
+    let offset = req.params.offset;
+
+    if (!topic || !offset) {
+        res.json("no topic and/or offset");
+        return;
+    }
+
+    let cClient = new kafka.KafkaClient({ kafkaHost: KAFKA_HOST });
+    let payload = [
+        {
+            topic: topic,
+            offset: offset,
+            partition: 0
+        }
+    ];
+    var options = {
+        fromOffset: true
+    };
+    
+    
+    let kOffset = new kafka.Offset(cClient);
+    kOffset.fetchLatestOffsets([topic], (err, offsets) => {
+        if (err) {
+            res.json(err);
+            return;
+        }
+
+        if (offsets[topic]['0'] <= offset) {
+            res.json("offsetOutOfRange");
+            return;
+        }
+
+        let consumer = new kafka.Consumer(cClient, payload, options);
+        consumer.on('message', (message) => {
+            if (message.offset == offset) {
+                res.json(message);
+                consumer.close(true);
+            }
+        });
+
+        consumer.on('error', (err) => {
+            res.json(err);
+            consumer.close(true);
+        });
     });
 });
 
