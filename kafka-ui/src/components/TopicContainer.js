bf64b8c32f5c31d3ad50cd540f6d713cdee4dced
moohh <mo051191@yahoo.com>
2019-10-05 17:27:14 -0400

    additional styling

--- kafka-ui/src/components/TopicContainer.js
+++ kafka-ui/src/components/TopicContainer.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+
 import styles from './styles'
 
 class TopicContainer extends Component {
@@ -10,9 +15,15 @@ class TopicContainer extends Component {
             <main className={this.props.classes.content}>
                 <div className={this.props.classes.toolbar}></div>
                 <Typography variant="h6">{this.props.topicName}</Typography>
-                {this.props.messages.map((m, index) => (
-                    this.props.topicName === m.topic ? <Typography key={index}>{m.value}</Typography> : null
-                ))}
+                <List>
+                    {this.props.messages.map((m, index) => (
+                        this.props.topicName === m.topic ?
+                            <ListItem key={index}>
+                                <ListItemText key={index}>{m.value}</ListItemText>
+                            </ListItem>
+                            : null
+                    ))}
+                </List>
             </main>
         )
     }
