5c0cf0d85d2518a6dc5442ceb906a2fb449441fe
moohh <mo051191@yahoo.com>
2019-10-06 00:15:43 -0400

    working ui

--- kafka-ui/src/components/TopicContainer.js
+++ kafka-ui/src/components/TopicContainer.js
@@ -5,6 +5,10 @@ import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import Icon from '@material-ui/core/Icon';
+import Container from '@material-ui/core/Container';
 
 import styles from './styles'
 
@@ -12,18 +16,41 @@ class TopicContainer extends Component {
 
     render() {
         return (
-            <main className={this.props.classes.content}>
+            <main className={this.props.classes.content} style={{height: "100vh"}}>
                 <div className={this.props.classes.toolbar}></div>
-                <Typography variant="h6">{this.props.topicName}</Typography>
-                <List>
-                    {this.props.messages.map((m, index) => (
-                        this.props.topicName === m.topic ?
-                            <ListItem key={index}>
-                                <ListItemText key={index}>{m.value}</ListItemText>
-                            </ListItem>
-                            : null
-                    ))}
-                </List>
+                <Container>
+                    <Typography variant="h6">{this.props.topicName}</Typography>
+                    <Typography variant="h6">{this.props.messages.length}</Typography>
+                    <List>
+                        {this.props.messages.map((m, index) => (
+                            this.props.topicName === m.topic ?
+                                <ListItem key={index}>
+                                    <ListItemText key={index}>{m.value}</ListItemText>
+                                </ListItem>
+                                : null
+                        ))}
+                    </List>
+                </Container>
+                {this.props.topicName === '' ? null :
+                <Container style={{position: "absolute",bottom: 0}}>
+                    <TextField
+                            id="outlined-name"
+                            label="send message..."
+                            margin="normal"
+                            variant="outlined"
+                            style={{margin: 8, width: "60vw"}}
+
+                            />
+                        <Button
+                            variant="contained"
+                            color="primary"
+                            endIcon={<Icon>send</Icon>}
+                            style={{height: "56px", marginTop: "8px"}}
+                            >
+                            Send
+                        </Button>
+                </Container>
+                }
             </main>
         )
     }
