888fc3a10f402d3084a44a21d18912af483d4b82
Mohammed Hussain <moohh91@gmail.com>
2020-12-21 00:33:43 -0500

    docker build files

--- .build/docker-build.sh
+++ .build/docker-build.sh
@@ -2,18 +2,24 @@ rm -rf ./sandbox
 
 mkdir sandbox
 cp Dockerfile ./sandbox/Dockerfile
+cp .dockerignore ./sandbox/.dockerignore
+cp kafka_2.12-2.3.0.tgz ./sandbox/kafka_2.12-2.3.0.tgz
 cp start-kafka-server.sh ./sandbox/start-kafka-server.sh
+
 cd ./sandbox
 
-#git clone
+git clone --single-branch --branch develop git@github.com:mhhussain/kafkabox.git
+
 cd ./kafkabox
 cd ./api
 yarn install
+
 cd ../ui
 yarn install
+
 yarn run build
 
-cd ..
+cd ../..
 
 docker build -t moohh/kafkabox:5.0.1-alpha .
 docker push moohh/kafkabox:5.0.1-alpha
